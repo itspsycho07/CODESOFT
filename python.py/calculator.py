322cb550839d5ff9ee5be86d841d27786e73721c
Anindya Mukherjee <97776159+itspsycho07@users.noreply.github.com>
2023-10-01 02:02:58 +0530

    Add files via upload

---
+++ python.py/calculator.py
@@ -0,0 +1,48 @@
+# Function to perform addition
+def add(x, y):
+    return x + y
+
+# Function to perform subtraction
+def subtract(x, y):
+    return x - y
+
+# Function to perform multiplication
+def multiply(x, y):
+    return x * y
+
+# Function to perform division
+def divide(x, y):
+    if y == 0:
+        return "Cannot divide by zero!"
+    return x / y
+
+# Main calculator function
+def calculator():
+    print("Options:")
+    print("Enter 'add' for addition")
+    print("Enter 'subtract' for subtraction")
+    print("Enter 'multiply' for multiplication")
+    print("Enter 'divide' for division")
+
+    operation = input("Enter operation choice: ")
+
+    if operation not in ('add', 'subtract', 'multiply', 'divide'):
+        print("Invalid operation choice")
+        return
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if operation == 'add':
+        result = add(num1, num2)
+    elif operation == 'subtract':
+        result = subtract(num1, num2)
+    elif operation == 'multiply':
+        result = multiply(num1, num2)
+    elif operation == 'divide':
+        result = divide(num1, num2)
+
+    print("Result: ", result)
+
+# Call the calculator function
+calculator()
